1ebe5f741b330c8a10741c0aa8dd40b82776cc19
tomcairns1 <t.cairns97@gmail.com>
2020-04-02 13:20:27 -0400

    Added map div, having trouble reading json data

--- js/visualization.js
+++ js/visualization.js
@@ -4,4 +4,40 @@
 
   console.log("Hello, world!");
 
+  // Create the map background
+  width = 821;
+  height = 520;
+
+  var mapsvg = d3
+ 	 .select('#map-container')
+ 	 .append('svg');
+
+  var projection = d3
+ 	 .geoAlbersUsa()
+ 	 .translate(width / 2, height / 2)
+ 	 .scale(width);
+
+  var path = d3.geoPath().projection(projection);
+
+  d3.json('data/us.json', function(us) {
+ 	 drawMap(us)
+ });
+
+ function drawMap(us) {
+ 	console.log(us)
+
+ 	var mapGroup = mapsvg.append('g').attr('class', 'mapGroup');
+
+ 	mapGroup
+ 	.append('g')
+ 	.attr('id', 'states')
+ 	.selectAll('path')
+ 	.data(topojson.feature(us, us.objects.states).features)
+ 	.enter()
+ 	.append('path')
+ 	.attr('d', path)
+ 	.attr('class', 'state');
+
+ }
+
 })());
