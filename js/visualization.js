80e505276dac5802111d3d31dd14223a0fe79625
Sid Radhakrishnan <sidrad@gmail.com>
2020-04-03 15:54:59 -0400

    Updated vis.js to display U.S. map

--- js/visualization.js
+++ js/visualization.js
@@ -4,40 +4,49 @@
 
   console.log("Hello, world!");
 
-  // Create the map background
-  width = 821;
-  height = 520;
-
-  var mapsvg = d3
- 	 .select('#map-container')
- 	 .append('svg');
-
+  var width = 960;
+  var height = 500;
+  
+  var svg = d3
+	.select("#map-container")
+	.append("svg")
+	.attr("width", width)
+	.attr("height", height);
+  
   var projection = d3
- 	 .geoAlbersUsa()
- 	 .translate(width / 2, height / 2)
- 	 .scale(width);
-
+	.geoAlbersUsa()
+	.translate([width / 2, height / 2])
+	.scale(width);
+  
   var path = d3.geoPath().projection(projection);
-
-  d3.json('data/us.json', function(us) {
- 	 drawMap(us)
- });
-
- function drawMap(us) {
- 	console.log(us)
-
- 	var mapGroup = mapsvg.append('g').attr('class', 'mapGroup');
-
- 	mapGroup
- 	.append('g')
- 	.attr('id', 'states')
- 	.selectAll('path')
- 	.data(topojson.feature(us, us.objects.states).features)
- 	.enter()
- 	.append('path')
- 	.attr('d', path)
- 	.attr('class', 'state');
-
- }
+  
+  d3.json("data/us.json", function(us) {
+	//Error
+	drawMap(us);
+  });
+  
+  function drawMap(us) {
+	var mapGroup = svg.append("g").attr("class", "mapGroup");
+  
+	mapGroup
+	  .append("g")
+	  // .attr("id", "states")
+	  .selectAll("path")
+	  .data(topojson.feature(us, us.objects.states).features)
+	  .enter()
+	  .append("path")
+	  .attr("d", path)
+	  .attr("class", "states");
+  
+	mapGroup
+	  .append("path")
+	  .datum(
+		topojson.mesh(us, us.objects.states, function(a, b) {
+		  return a !== b;
+		})
+	  )
+	  .attr("id", "state-borders")
+	  .attr("d", path);
+  }
 
 })());
