a2ba083c2123e3ce23388331b08c0aab34d87f83
Sid Radhakrishnan <sidrad@gmail.com>
2020-04-05 19:09:42 -0400

    Fixed styling of barchart

--- js/barchart.js
+++ js/barchart.js
@@ -1,6 +1,7 @@
 // This is the javascript for the barcharts
-var width = 800,
-    height = 300;
+var margin = 50,
+    width = 900-margin*2,
+    height = 400-margin*2;
 
 var x = d3.scaleBand().rangeRound([0, width], .05);
 
@@ -14,9 +15,11 @@ var barpadding = 8;
 
 var svg = d3.select("#chart-container")
     .append("svg")
-    .attr("width", width + 70)
-    .attr("height", height + 110);
-
+    .attr("width", width + 2 * margin)
+    .attr("height", height + 2 * margin)
+    .append("g")
+    .attr("transform", `translate(${margin},${margin})`)
+    
 function render(csv){
     var csvPath = "data/hive\ plant\ species\ data/"+csv+".csv"
     d3.csv(csvPath, function(err, data) {
@@ -29,24 +32,12 @@ function render(csv){
 
       svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", "-.55em")
-        .attr("transform", "rotate(-90)" );
+        .attr("transform", `translate(0,${height})`)
+        .call(xAxis);
 
     svg.append("g")
         .attr("class", "y axis")
-        .attr("transform", "translate(" + (width+20) + ",0)")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Value ($)");
+        .call(yAxis);
 
       svg.selectAll("bar")
           .data(data)
